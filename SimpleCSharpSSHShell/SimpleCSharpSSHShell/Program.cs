04956ca375fc7df76b5267b0b2a784e792bc037c
Jonathan Herrmann <jonathan.daniel.herrmann@gmail.com>
2019-08-26 21:24:15 +0200

    - Added AssistantOne project to build first simple Assistant
    - Added named Pipe Server and Client example to show basic infrastructure point
    -

--- SimpleCSharpSSHShell/SimpleCSharpSSHShell/Program.cs
+++ SimpleCSharpSSHShell/SimpleCSharpSSHShell/Program.cs
@@ -15,6 +15,8 @@ namespace SimpleCSharpSSHShell
             // locate any commands in the assembly (or use an IoC container, or whatever source)
             var commands = GetCommands();
 
+            var NamedPipeServerInstance = new NamedPipeServer();
+
             // then run them.
             return ConsoleCommandDispatcher.DispatchCommand(commands, args, Console.Out);
         }
